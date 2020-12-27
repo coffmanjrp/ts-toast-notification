326a6aa05cde05203303b78c2a93728e274be18d
coffmanjrp <coffmanjrp@gmail.com>
2020-12-27 14:58:13 -0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+var button = document.getElementById('button');
+var toasts = document.getElementById('toasts');
+var messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+    'Message Five',
+];
+var types = ['info', 'success', 'error'];
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)];
+}
+function getRandomType() {
+    return types[Math.floor(Math.random() * messages.length)];
+}
+function createNotification(message, type) {
+    if (message === void 0) { message = null; }
+    if (type === void 0) { type = null; }
+    var notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.classList.add(type || getRandomType());
+    notif.innerText = message || getRandomMessage();
+    toasts.appendChild(notif);
+    setTimeout(function () {
+        notif.remove();
+    }, 3000);
+}
+button.addEventListener('click', function () { return createNotification(); });
